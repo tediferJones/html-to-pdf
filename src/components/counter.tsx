28a51a01669f317e92feb0a78584e0cfd0a32bff
ted <theo_drz@live.com>
2023-07-31 20:06:17 -0400

    tried a couple pdfViewers, and tried to import old component into astro

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,5 +1,19 @@
+import { useState } from 'react';
+// import PdfViewer from 'pdf-react';
+// import { SimplePdfViewerModule } from 'simple-pdf-viewer';
+// import { Document } from 'react-pdf';
+// import reactPdf from 'react-pdf';
+// const { Document } = reactPdf;
+// <Document file='https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf' />
+// import htmlPdfClient from 'html-pdf-client';
+
 export default function Counter() {
+  const [state, setState] = useState(1);
   return (
-    <h1>COUNTER COMPONENT</h1>
+    <div>
+      <h1>Counter: {state}</h1>
+      <button onClick={() => setState(state + 1)}>Increment</button>
+      <button onClick={() => setState(state - 1)}>Dincrement</button>
+    </div>
   )
 }
---
+++ src/pdf.js
@@ -0,0 +1,5 @@
+// const pdfjs = require('pdfjs-dist')
+
+console.log('HELLO WORLD')
+console.log(pdfjs)
+console.log(pdfjsLib)
